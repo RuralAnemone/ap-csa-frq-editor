32cfb41ef235840aa71d2d7f29550f8e7ec4d601
Isaiah W <64103005+RuralAnemone@users.noreply.github.com>
2023-05-11 18:49:42 +0000

    real init, make biolerplate idk

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = process.env['PORT'] || 3000;
+
+app.use("/", express.static(path.join(__dirname, 'public')));
+
+app.use((req, res) => {
+	res.status(404).sendFile(`${process.cwd()}/public/404.html`)
+});
+
+app.listen(port);
